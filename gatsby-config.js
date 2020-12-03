5c67d4f7575aa355a78b3bb0b9edb1263c62397a
janniks <jannik.siebert@gmail.com>
2020-12-03 18:12:57 +0100

    Add meta images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,16 @@
-require(`dotenv`).config({
-  path: `.env`,
+require('dotenv').config({
+  path: '.env',
 })
 
 module.exports = {
   siteMetadata: {
     siteTitle: 'early return',
-    siteTitleAlt: `Coding Fun`,
+    siteTitleAlt: 'Coding Fun',
+    banner: '/banner.png',
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      resolve: '@lekoarts/gatsby-theme-minimal-blog',
       options: {
         blogPath: '/',
         navigation: [
@@ -20,42 +21,42 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/earlyreturnio`,
+            name: 'Twitter',
+            url: 'https://twitter.com/earlyreturnio',
           },
           {
-            name: `RSS`,
-            url: `https://earlyreturn.io/rss.xml`,
+            name: 'RSS',
+            url: 'https://earlyreturn.io/rss.xml',
           },
         ],
       },
     },
-    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `early return`,
-        short_name: `early return`,
-        description: `Returning Early`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
+        name: 'early return',
+        short_name: 'early return',
+        description: 'Returning Early',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#6B46C1',
+        display: 'standalone',
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
   ],
 }
